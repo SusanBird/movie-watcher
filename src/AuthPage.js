f0279c7118ff0152a39d425c2e8bd129a87b8ee1
SusanBird <susanbird391@gmail.com>
2022-05-23 21:36:32 -0700

    auth page set state, wrote functions

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { signInUser, signUpUser } from './services/supabase-utils';
 
-export default function AuthPage() {
+export default function AuthPage({ setCurrentUser }) {
+
+  const [signUpEmail, setSignUpEmail] = useState('');
+  const [signUpPassword, setSignUpPassword] = useState('');
+  const [signInEmail, setSignInEmail] = useState('');
+  const [signInPassword, setSignInPassword] = useState('');
+
+  function clearForms() {
+    setSignInEmail('');
+    setSignInPassword('');
+    setSignUpEmail('');
+    setSignUpPassword('');
+  }
+
+  async function handleSignUp(e) {
+    e.preventDefault(); 
+
+    const user = await signUpUser(signUpEmail, signUpPassword);
+    setCurrentUser(user);
+    clearForms();
+  }
+
+  async function handleSignIn(e) {
+    e.preventDefault(); 
+
+    const user = await signInUser(signInEmail, signInPassword);
+    setCurrentUser(user);
+    clearForms();
+  }
+
   return (
     <div>AuthPage</div>
   );
---
+++ src/services/supabase-utils.js
@@ -0,0 +1,14 @@
+import { client } from './client';
+
+export async function signUpUser(email, password) {
+  const response = await client.auth.signUp({ email, password });
+
+  return response.user;
+}
+
+export async function signInUser(email, password) {
+  const response = await client.auth.signIn({ email, password });
+  
+  return response.user;
+}
+  
